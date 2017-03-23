be13a2fde532ff5535c25990fa99d282355be1f5
Jeremy Garner <jegarne@gmail.com>
2017-03-22 20:52:55 -0500

    initial commit

---
+++ src/UserSignup/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UserSignup.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserSignup.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index(User user)
+        {
+            //TODO 3: update this to get all users from your UserData class
+            // and return it to the view.  Update the view to display some user data.
+            if (user == null) user = new User();
+            return View(user);
+        }
+
+        // TODO 4: add a details controller and view that takes a single userid,
+        // gets that user object from UserData, returns it to the details view
+        // where it is displayed
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new User());
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user, string verify)
+        {
+            if (user.Password == verify && !String.IsNullOrEmpty(user.Username))
+            {
+                // return RedirectToAction("Index", user);
+                return RedirectToAction("Index", new { Username = user.Username } );
+            }
+            else
+            {
+                ViewBag.PasswordError = user.Password != verify ? "Your passwords must match" : "";
+                ViewBag.UsernameError = String.IsNullOrEmpty(user.Username) ? "You must enter a username" : "";
+                return View(user);
+            }
+        }
+    }
+}
---
+++ src/UserSignup/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserSignup.Models
+{
+    public class User
+    {
+        // TODO 1: Add UserId, CreateDate and a few more properties of your choosing.  Update the Add and Index views 
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
+        // add a constructor to set the CreateDate when a new user is instantiated        
+    }
+}
