cca02c5324d5b96cb74bdcce309b706c12bbdd6e
sharon-launchcode <sharon.launchcode@gmail.com>
2018-03-08 16:03:25 -0500

    added vampiire method of add user per instructions

--- src/UserSignup/Models/UserData.cs
+++ src/UserSignup/Models/UserData.cs
@@ -12,7 +12,7 @@ namespace UserSignup.Models
             {
                 Username = "Username1",
                 Password = "password1",
-                Email = "email1@gogus.com"
+                Email = "email1@bogus.com"
             },
             new User
             {
@@ -23,6 +23,17 @@ namespace UserSignup.Models
         };
         //TODO 2: instantiate a few new users in code here and add them to your users list
         //then write methods to add users to your list, return all users and return a user by UserId
+        public void AddUser(User user)
+        {
+            users.Add(user);
+            //using vampiire example
+        }
 
+        public void RemoveUser(User user)
+        {
+            // vampiire example check if user exists first?
+            users.Remove(user);
+        }
+        public List<User> GetUsers() => users;
     }
 }
