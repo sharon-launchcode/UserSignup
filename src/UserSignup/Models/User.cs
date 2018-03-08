7bbc7064216c1df1a891070ecf47af2e6ddd1acf
sharon-launchcode <sharon.launchcode@gmail.com>
2018-03-08 13:04:15 -0500

    starting to incorporate vampiire notes from class

--- src/UserSignup/Models/User.cs
+++ src/UserSignup/Models/User.cs
@@ -7,9 +7,20 @@ namespace UserSignup.Models
 {
     public class User
     {
+        // TODO 1 from vampiire notes
+        // create private FIELDS accessed through (protected by) public PROPERTIES 
+        private string username;
+        private string email;
+        private string password;
         // TODO 1: Add UserId, CreateDate and a few more properties of your choosing.  Update the Add and Index views 
         public string Username { get; set; }
-        public string Email { get; set; }
+        public string Email {
+            get => email;
+
+            set {
+                email = value;
+            }
+        }
         public string Password { get; set; }
         //get but not set for ID should it be public or private?
         public int UserId { get; }
@@ -24,8 +35,7 @@ namespace UserSignup.Models
         }
 
         // add a constructor to set the CreateDate when a new user is instantiated   
-
-
+        //above constructor is added but I need to figure out what to do with the get
     }
 
 }
