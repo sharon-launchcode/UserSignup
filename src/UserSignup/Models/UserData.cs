34409f82d1874272ab970028853c2eaa163f1fa6
sharon-launchcode <sharon.launchcode@gmail.com>
2018-03-08 15:41:30 -0500

    thank you John

--- src/UserSignup/Models/UserData.cs
+++ src/UserSignup/Models/UserData.cs
@@ -7,7 +7,18 @@ namespace UserSignup.Models
 {
     public class UserData
     {
-        private List<User> users = new List<User>();
+        private List<User> users = new List<User> {
+            new User
+            {
+                Password = "password1",
+                Email = "email1"
+            },
+            new User
+            {
+                Password = "password1",
+                Email = "email1"
+            }
+        };
         //TODO 2: instantiate a few new users in code here and add them to your users list
         //then write methods to add users to your list, return all users and return a user by UserId
 
